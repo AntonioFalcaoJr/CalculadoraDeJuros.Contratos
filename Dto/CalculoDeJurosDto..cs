5301975985d59b747c2fbabca5741ab1a9705fb0
Antonio Falcao <arfj@edu.univali.br>
2019-11-03 21:03:36 -0300

    Incluindo CalculoDeJurosDto

---
+++ Dto/CalculoDeJurosDto..cs
@@ -0,0 +1,9 @@
+﻿using CalculadoraDeJuros.Contratos.Errors.Hiper.Domain.Base.Errors;
+
+namespace CalculadoraDeJuros.Contratos.Dto
+{
+    public class CalculoDeJurosDto : BaseError
+    {
+        public decimal Valor { get; set; }
+    }
+}
